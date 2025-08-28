5dfbd6f33e8a06f7478f1131cd5605f6739ff14e
João Carvalho <joaogabrielsc3@gmail.com>
2025-08-28 16:38:37 -0300

    Update and rename Ex. 6.cpp to ex6/ex6.cpp

--- ex6/ex6.cpp
+++ ex6/ex6.cpp
@@ -6,12 +6,12 @@ string diaSemana(int numero) {
     switch (numero) {
         case 1: return "Domingo";
         case 2: return "Segunda-feira";
-        case 3: return "Terça-feira";
+        case 3: return "TerÃ§a-feira";
         case 4: return "Quarta-feira";
         case 5: return "Quinta-feira";
         case 6: return "Sexta-feira";
-        case 7: return "Sábado";
-        default: return "Número inválido (digite de 1 a 7)";
+        case 7: return "SÃ¡bado";
+        default: return "NÃºmero invÃ¡lido (digite de 1 a 7)";
     }
 }
 
